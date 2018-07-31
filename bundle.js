9bf685ffc39648c46332341bedfc3ac742c22cc1
goldenmark <marchioro@me.com>
2018-07-31 19:40:27 +0200

    Prevent Jekyll build

--- bundle.js
+++ bundle.js
@@ -1,3 +1,21 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+let {Phrase} = require("golden-palindrome");
+
+let string = prompt("Please enter a string for palindrome testing:");
+let phrase = new Phrase(string);
+
+if (phrase.palindrome()) {
+  alert(`"${phrase.content}" is a palindrome!`);
+} else {
+  alert(`"${phrase.content}" is not a palindrome.`);
+}
+
+},{"golden-palindrome":2}],2:[function(require,module,exports){
+module.exports = {
+  Phrase: Phrase,
+  TranslatedPhrase: TranslatedPhrase
+}
+
 // Adds `reverse` to all strings.
 String.prototype.reverse = function() {
   return Array.from(this).reverse().join("");
@@ -14,7 +32,7 @@ Array.prototype.last = function() {
 
 // Defines a Phrase object.
 function Phrase(content) {
-  this.content = content
+  this.content = content;
 
   this.letters = function letters(string) {
     return Array.from(string).filter(c => c.match(/[a-z]/i)).join("");
@@ -45,6 +63,4 @@ function TranslatedPhrase(content, translation) {
 
 TranslatedPhrase.prototype = new Phrase();
 
-frase = new TranslatedPhrase("recognize", "Madam, I'm Adam.");
-console.log(frase.processedContent(frase.translation));
-console.log(frase.palindrome());
+},{}]},{},[1]);
